fff1032a54be2f3ffe9d76e362af40a8c7d769c2
chriswest <apple@Chris-Wests-New-MacBook-Pro.local>
2013-07-05 16:30:32 +0800

    changes

--- website/urls.py
+++ website/urls.py
@@ -6,7 +6,3 @@ import views
 urlpatterns = patterns('django.views.generic.simple',
     (r'^$', 'direct_to_template', {'template': 'index_en.html',}),
 )
-
-urlpatterns += patterns('',
-    url(r'^update/$', views.userEdit, name="useredit"),
-)
--- website/views.py
+++ website/views.py
@@ -11,51 +11,7 @@ from django.contrib.auth.decorators import login_required
 import re, htmlentitydefs
 from time import time
 
-from bs4 import BeautifulSoup
-
 from website.models import *
-
-
-@csrf_exempt
-@login_required
-def userEdit(request):
-    
-    if not request.user.is_superuser:
-        return Http404
-    else:
-    
-        if request.method == 'POST':
-        
-            # find the file, open and replace with the new version
-            template_name = "index_%s.html" % request.POST['lang']
-            f = open(os.path.join('templates/', template_name), 'r')
-            
-            # archive the old file
-            content = f.read()
-            new_name = "index_%s.%s.html" % (request.POST['lang'], time())
-            directory = "%s/templates/archives/%s/" % (settings.PROJECT_PATH, request.user)
-            full_path = "".join((directory, new_name))
-            new_file = open(full_path, 'w')
-            new_file.write(content)
-            new_file.close()
-            f.close()
-            
-            
-            file = open(os.path.join('templates/', template_name), 'r')
-            # now do the new stuff
-            soup = BeautifulSoup(file)  
-            soup.find(id="container").string = smart_str(request.POST['new'])       
-            
-                    
-            # finally, we'll overwrite the new file
-            new_html = smart_unicode(unescape(soup.prettify())) 
-            file = open(os.path.join('templates/', template_name), mode='w+')
-            file.write(new_html.encode('utf8'))
-            file.close()
-        else:
-            return Http404
-                
-    return HttpResponse('true')
 
 
 def get_drafts(request, user):
